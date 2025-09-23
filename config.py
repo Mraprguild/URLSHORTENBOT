945bb37546a8850160238b2d4c5d1f5b88f7d49c
Mraprguild8133 <mraprguild06@gmail.com>
2025-09-23 15:36:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# Replace with your actual credentials from my.telegram.org and @BotFather
+# IMPORTANT: Keep this file private and do not share it publicly.
+API_ID = "API_ID"
+API_HASH = "API_HASH"
+BOT_TOKEN = "BOT_TOKEN"

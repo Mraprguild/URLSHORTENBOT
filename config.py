7e51cc2713e9a6de0392ac021a0ac8d6c6617e32
Mraprguild8133 <mraprguild06@gmail.com>
2025-09-24 14:12:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
 # API Keys for various services
 BITLY_TOKEN = os.getenv('BITLY_TOKEN', '')
 CUTTLY_API = os.getenv('CUTTLY_API', '')
-GPLINKS_API = os.getenv('GPLINKS_API', 'YOUR_GPLINKS_API_KEY_HERE')  # Make sure this is set
+GPLINKS_API = os.getenv('GPLINKS_API', '')
+
+# Welcome image URL
+WELCOME_IMAGE_URL = "https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+
+# Server port for Render
+PORT = int(os.environ.get('PORT', 5000))
 
 # Supported URL shortening services
 SUPPORTED_SERVICES = {
